067a6367f638c20e31098b741e0e43ff100368b9
unknown <roger.rajala@gmail.com>
2019-02-18 08:41:38 +0200

    :D

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Routing from './components/Routing';
-import Header from './components/Header';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyles = createGlobalStyle`
-    body {
-        @import url('https://fonts.googleapis.com/css?family=Josefin+Sans|Open+Sans');
-        font-family: 'Open Sans', sans-serif;
-    }
-`;
+import React, { Component } from "react";
+import styled from "styled-components";
+import Routing from "./components/Routing";
+import Header from "./components/Header";
 
 const AppWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
 `;
 
 const AppBody = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 80px;
-  height: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 80px;
+    height: 100%;
 `;
 
 class App extends Component {
-  render() {
-    return (
-      <AppWrapper>
-        <Header />
-        <AppBody>
-          <Routing />
-        </AppBody>
-      </AppWrapper>
-    );
-  }
+    render() {
+        return (
+            <AppWrapper>
+                <Header />
+                <AppBody>
+                    <Routing />
+                </AppBody>
+            </AppWrapper>
+        );
+    }
 }
 
 export default App;
