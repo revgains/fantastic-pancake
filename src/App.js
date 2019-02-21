4dbd41c895b94f1cc0061602fe4ef3c4ca5ab48b
unknown <roger.rajala@gmail.com>
2019-02-21 15:22:33 +0200

    fontawesome icon +

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import styled from "styled-components";
 import Routing from "./components/Routing";
 import Header from "./components/Header";
-
-const AppWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-`;
-
-const AppBody = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 80px;
-    height: 100%;
-`;
-
-class App extends Component {
-    render() {
-        return (
-            <AppWrapper>
-                <Header />
-                <AppBody>
-                    <Routing />
-                </AppBody>
-            </AppWrapper>
-        );
-    }
-=======
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Routing from './components/Routing';
-import Header from './components/Header';
-import { createGlobalStyle } from 'styled-components';
-import Footer from './components/Footer';
 
 const GlobalStyles = createGlobalStyle`
     body {
