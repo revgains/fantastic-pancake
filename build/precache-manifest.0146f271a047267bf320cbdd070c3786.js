44d19e0e5b4fdca59853784d63e97892fc92cc2e
unknown <roger.rajala@gmail.com>
2019-02-22 15:32:59 +0200

    test

---
+++ build/precache-manifest.0146f271a047267bf320cbdd070c3786.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fffb1811a4d24341007b777efb894ab",
+    "url": "/static/media/hiking-card-img.5fffb181.jpg"
+  },
+  {
+    "revision": "4293b1d07586ed99ed10",
+    "url": "/static/css/main.9285747c.chunk.css"
+  },
+  {
+    "revision": "c59a50b627f367507bdf",
+    "url": "/static/js/1.c59a50b6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84fc190a48aca26a7082852c8b69c9d2",
+    "url": "/static/media/footerbg.84fc190a.png"
+  },
+  {
+    "revision": "72c405896ef399dc62d2a0bd79230ccf",
+    "url": "/static/media/rorvik-logo.72c40589.svg"
+  },
+  {
+    "revision": "3e7e642bbb11dc150454304261f591c9",
+    "url": "/static/media/fishing-card-img.3e7e642b.jpg"
+  },
+  {
+    "revision": "4293b1d07586ed99ed10",
+    "url": "/static/js/main.4293b1d0.chunk.js"
+  },
+  {
+    "revision": "11964860c88854329f491da8ce585d56",
+    "url": "/static/media/kayak-card-img.11964860.jpg"
+  },
+  {
+    "revision": "f958d58c09a787f8daa181b75594f500",
+    "url": "/static/media/klippan-card-img.f958d58c.jpg"
+  },
+  {
+    "revision": "0180469e5ee145fed394a43f60ce628f",
+    "url": "/static/media/paviljong-card-img.0180469e.jpg"
+  },
+  {
+    "revision": "d092bcfa8c31dc7215f584e6501fbb7c",
+    "url": "/static/media/sauna-card-img.d092bcfa.jpg"
+  },
+  {
+    "revision": "6ae88d7531c515da9b5b42b133394f66",
+    "url": "/static/media/sup-card-img.6ae88d75.jpg"
+  },
+  {
+    "revision": "5b2410af5ee242f592d4be664cdfda2b",
+    "url": "/index.html"
+  }
+];
