3c0d3e1b1b4817607b19a5dfecfe46fc4b9fef86
revgains <roger.rajala@gmail.com>
2019-02-22 20:36:38 +0200

    _redirects

--- src/components/Cabins.js
+++ src/components/Cabins.js
@@ -1,53 +1,53 @@
-import React from "react";
-import data from "../data/cabins.json";
-import Card from "./Card";
-import colors from "../theme/colors";
-import styled from "styled-components";
+import React from 'react';
+import data from '../data/cabins.json';
+import Card from './Card';
+import colors from '../theme/colors';
+import styled from 'styled-components';
 
 export const CabinsWrapper = styled.div`
-    display: flex;
-    background: ${colors.grey100};
-    width: 100%;
-    height: 1440px;
-    padding: 120px;
-    padding-top: 64px;
-    flex-direction: column;
-    flex: 1;
+  display: flex;
+  background: ${colors.grey100};
+  width: 100%;
+  height: 1440px;
+  padding: 120px;
+  padding-top: 64px;
+  flex-direction: column;
+  flex: 1;
 `;
 
 export const CardTitle = styled.h1`
-    @import url("https://fonts.googleapis.com/css?family=Josefin+Sans");
-    width: 327px;
-    height: 86px;
-    color: ${colors.dark};
-    font-size: 36px;
-    font-family: "Josefin Sans", sans-serif;
-    font-weight: 600;
-    padding-left: 12px;
+  @import url('https://fonts.googleapis.com/css?family=Josefin+Sans');
+  width: 327px;
+  height: 86px;
+  color: ${colors.dark};
+  font-size: 36px;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 600;
+  padding-left: 12px;
 `;
 
 export const CardWrapper = styled.div`
-    display: flex;
-    flex-flow: row wrap;
+  display: flex;
+  flex-flow: row wrap;
 `;
 const Cabins = () => {
-    return (
-        <CabinsWrapper>
-            <CardTitle>Accommodations in Rörvik</CardTitle>
-            <CardWrapper>
-                {data.cabins.map(cabin => {
-                    return (
-                        <Card
-                            name={cabin.name}
-                            beds={cabin.beds}
-                            description={cabin.description}
-                            image={cabin.image}
-                        />
-                    );
-                })}
-            </CardWrapper>
-        </CabinsWrapper>
-    );
+  return (
+    <CabinsWrapper>
+      <CardTitle>Accommodations in Rörvik</CardTitle>
+      <CardWrapper>
+        {data.cabins.map(cabin => {
+          return (
+            <Card
+              name={cabin.name}
+              beds={cabin.beds}
+              description={cabin.description}
+              image={cabin.image}
+            />
+          );
+        })}
+      </CardWrapper>
+    </CabinsWrapper>
+  );
 };
 
 export default Cabins;
